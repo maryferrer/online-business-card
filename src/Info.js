e8525c3c1a1f3223618732a5aa6658e31d3e9921
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-21 19:57:58 -0800

    create Info.js component including h1, website and contact information

---
+++ src/Info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Info() {
+    return (
+        <div className="Info">
+            <img src="./prof4.jpg" alt="Mary Ferrer" className="picture" />
+            <h1>Mary Ferrer</h1>
+            <h2>Frontend Developer</h2>
+            <a href="maryferrer.github.io" className="website">maryferrer.github.io</a>
+            <a href="mailto:mary.ferrer6@gmail.com" className="email"><i class="fas fa-envelope"></i>Email</a>
+        </div>
+    )
+}
+
+export default Info;
