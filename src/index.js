33dc40beb9172b4cd3be311ab4b1a1e5e47a4224
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-21 19:59:34 -0800

    Index.js with structure of page using components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Info from './Info';
+import About from './About';
+import Interests from './Interests';
+import Footer from './Footer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <section className="container">
+    <Info />
+      <article className="container2">
+        <About />
+        <Interests />
+      </article>
+      <Footer />
+    </section>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +23,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
