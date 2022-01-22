70b5a67521e70f06630bec5cb250e46de7080465
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-21 20:06:18 -0800

    update paragraph content

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div>
             <h3>About</h3>
-            <p>Quick learner and self-taught developer passionate about contributing to the Tech and Biotech industries with responsive, accesible UIs and high-quality code. I have 4+ years of experience in sales and customer-focused roles in Biotech and am able to craft solutions quickly and efficiently under pressure.</p>
+            <p>Quick learner and self-taught developer passionate about contributing to the tech and biotech industries with responsive, accesible UIs and high-quality code. I have 4+ years of experience in sales and customer-focused roles in Biotech and am able to craft solutions quickly and efficiently under pressure.</p>
         </div>
     )
 };
