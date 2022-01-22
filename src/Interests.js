fe52391ee634acdf11d95c21e3375227f348b2ba
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-21 19:57:24 -0800

    create Interests.js component with header and paragraph

---
+++ src/Interests.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Interests() {
+    return (
+        <div>
+            <h3>Interests</h3>
+            <p>Aside from Web Development, I'm incredibly fascinated by the ever-evolving technologies of the Tech and Biotech industries. With my professional background in Biotech, I aspire to use my programming skills to support scientific advances and improve the overall health of the world.</p>
+        </div>
+    )
+};
+
+export default Interests;
