e7088b828f8ff6ff9b725aa8f5b3351f9be84893
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-21 19:56:59 -0800

    create About.js component with about me header and paragraph

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h3>About</h3>
+            <p>Quick learner and self-taught developer passionate about contributing to the Tech and Biotech industries with responsive, accesible UIs and high-quality code. I have 4+ years of experience in sales and customer-focused roles in Biotech and am able to craft solutions quickly and efficiently under pressure.</p>
+        </div>
+    )
+};
+
+export default About;
