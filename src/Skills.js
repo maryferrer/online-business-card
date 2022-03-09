8a14d17126521fa484f928166245828188f3e69a
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-03-09 11:25:39 -0800

    create skills component

---
+++ src/Skills.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Skills() {
+    return (
+        <div>
+            <h3>Skills</h3>
+            <ul>
+                <li>React.js</li>
+                <li>JavaScript</li>
+                <li>CSS</li>
+                <li>HTML</li>
+                <li>Git/GitHub</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Skills;
