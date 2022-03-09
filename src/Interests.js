1df6812111c4d84321f38b42b4fec23b06c60549
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-03-09 11:24:50 -0800

    update interests paragraph

--- src/Interests.js
+++ src/Interests.js
@@ -4,7 +4,7 @@ function Interests() {
     return (
         <div>
             <h3>Interests</h3>
-            <p>Aside from Web Development, I'm incredibly fascinated by the ever-evolving technologies of the Tech and Biotech industries. With my professional background in Biotech, I aspire to use my programming skills to support scientific advances and improve the overall health of the world.</p>
+            <p>I'm fascinated by the ever-evolving technologies of the Tech and Biotech industries. With my professional background in Biotech, I aspire to use my Front-end skills to support scientific advances and improve the overall health of the world. I want to join a company with humanitarian values and goals.</p>
         </div>
     )
 };
