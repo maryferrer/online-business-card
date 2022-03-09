e17621da7aacf821a7a7572637c4370927b3ad94
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-03-09 11:25:07 -0800

    add resume button

--- src/Info.js
+++ src/Info.js
@@ -3,11 +3,14 @@ import React from 'react';
 function Info() {
     return (
         <div className="Info">
-            <img src="./prof4.jpg" alt="Mary Ferrer" className="picture" />
+            <img src="./Amterdam-boat2.jpg" alt="Mary Ferrer" className="picture" />
             <h1>Mary Ferrer</h1>
             <h2>Front-end Developer</h2>
             <a href="https://maryferrer.github.io" className="website" target="_blank" rel="noreferrer">maryferrer.github.io</a>
-            <a href="mailto:mary.ferrer6@gmail.com" className="email"><i class="fas fa-envelope"></i>Email</a>
+            <section className="info--links">
+                <a href="mailto:mary.ferrer6@gmail.com" className="email"><i class="fas fa-envelope"></i>Email</a>
+                <a href="/mferrer resume.pdf" className="resume" target="_blank">Resume</a>
+            </section>
         </div>
     )
 }
